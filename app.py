724d03cfe5e108eed646b532a63d0d07425abb0b
Gins Zheng <ginszheng@163.com>
2019-08-28 00:04:07 +0800

    s2

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    randomNum = random.randint(int('0x4e00', 16), int('0x9fa5', 16))
+    randomUnicode = "\\u%x" % randomNum
+    randomWord = str(randomUnicode.encode('utf-8').decode('unicode_escape'))
+
+    blackEggProbability = str(random.randint(1, 333))
+
+    return jsonify({'word': randomWord, 'probability': blackEggProbability})
 
 
 if __name__ == '__main__':
