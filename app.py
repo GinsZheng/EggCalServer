047acbfeab22c1c6ac6f2acaefa0ffb0ac797bba
Gins Zheng <ginszheng@163.com>
2019-08-29 00:02:47 +0800

    s3

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ def hello_world():
     randomUnicode = "\\u%x" % randomNum
     randomWord = str(randomUnicode.encode('utf-8').decode('unicode_escape'))
 
-    blackEggProbability = str(random.randint(1, 333))
+    blackEggProbability = str(random.randint(1, 1))
+    imageLink = "https://www.baidu.com/img/bd_logo1.png?qua=high"
+    imageLink = "http://127.0.0.1:5000/static/images/imageInBlackEgg.png"
 
-    return jsonify({'word': randomWord, 'probability': blackEggProbability})
+    return jsonify({'word': randomWord, 'probability': blackEggProbability, 'link': imageLink})
 
 
 if __name__ == '__main__':
